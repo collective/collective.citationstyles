1537cb228bd6646f2f30a4350ae3fb77003ef9e3
cewing <cris@crisewing.com>
2013-09-04 13:14:05 -0700

    * set up test harness with some bibliographic content in place * add example bibtex source file to populate the site * add metadata dependency on CMFBibAT so we can install it and use it in test setup.

--- src/collective/citationstyles/testing.py
+++ src/collective/citationstyles/testing.py
@@ -1,12 +1,22 @@
+import os.path
+
 from plone.app.testing import PloneSandboxLayer
 from plone.app.testing import applyProfile
 from plone.app.testing import PLONE_FIXTURE
 from plone.app.testing import IntegrationTesting
 from plone.app.testing import FunctionalTesting
+from plone.app.testing import login
+from plone.app.testing import logout
+from plone.app.testing import setRoles
+from plone.app.testing import TEST_USER_NAME
+from plone.app.testing import TEST_USER_ID
 
 from plone.testing import z2
 
 from zope.configuration import xmlconfig
+
+
+BIBTEX_TEST_BIB = os.path.join(os.path.dirname(__file__), 'tests/source.bib')
 
 
 class CollectivecitationstylesLayer(PloneSandboxLayer):
@@ -21,16 +31,28 @@ class CollectivecitationstylesLayer(PloneSandboxLayer):
             collective.citationstyles,
             context=configurationContext
         )
+        import Products.ATExtensions
+        xmlconfig.file(
+            'configure.zcml',
+            Products.ATExtensions,
+            context=configurationContext
+        )
 
-        # Install products that use an old-style initialize() function
-        #z2.installProduct(app, 'Products.PloneFormGen')
+        z2.installProduct(app, 'Products.CMFBibliographyAT')
 
-#    def tearDownZope(self, app):
-#        # Uninstall products installed above
-#        z2.uninstallProduct(app, 'Products.PloneFormGen')
+    def tearDownZope(self, app):
+        z2.uninstallProduct(app, 'Products.CMFBibliographyAT')
 
     def setUpPloneSite(self, portal):
         applyProfile(portal, 'collective.citationstyles:default')
+        setRoles(portal, TEST_USER_ID, ['Manager'])
+        login(portal, TEST_USER_NAME)
+        bf_id = portal.invokeFactory("BibliographyFolder", id="bib_folder")
+        bib_folder = portal[bf_id]
+        bib_source = open(BIBTEX_TEST_BIB, 'r').read()
+        bib_folder.processImport(bib_source, 'source.bib')
+        setRoles(portal, TEST_USER_ID, ['Member'])
+        logout()
 
 COLLECTIVE_CITATIONSTYLES_FIXTURE = CollectivecitationstylesLayer()
 COLLECTIVE_CITATIONSTYLES_INTEGRATION_TESTING = IntegrationTesting(
