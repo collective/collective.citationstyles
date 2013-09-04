6e343b19aee8e8405f2243231b07f5f555000c44
cewing <cris@crisewing.com>
2013-09-04 15:18:20 -0700

    tests for our json view

--- src/collective/citationstyles/tests/test_rendering.py
+++ src/collective/citationstyles/tests/test_rendering.py
@@ -1,15 +1,88 @@
 import unittest2 as unittest
+import json
 
+from plone.app.testing import login
+from plone.app.testing import logout
+from plone.app.testing import setRoles
+from plone.app.testing import TEST_USER_NAME
+from plone.app.testing import TEST_USER_ID
+
+from collective.citationstyles.browser.json import CitationJSONView
 from collective.citationstyles.testing import \
     COLLECTIVE_CITATIONSTYLES_INTEGRATION_TESTING
 
 
-class TestRendering(unittest.TestCase):
+
+
+class TestJSONView(unittest.TestCase):
 
     layer = COLLECTIVE_CITATIONSTYLES_INTEGRATION_TESTING
 
     def setUp(self):
         self.app = self.layer['app']
         self.portal = self.layer['portal']
+        self.bib_folder = self.portal.bib_folder
 
+    def _createCollection(self):
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+        login(self.portal, TEST_USER_NAME)
+        tid = self.portal.invokeFactory('Collection', 'bibtopic')
+        topic = self.portal[tid]
+        logout()
+        setRoles(self.portal, TEST_USER_ID, ['Member'])
+        return topic
 
+    def getView(self, ctxt):
+        view = None
+        try:
+            view = ctxt.restrictedTraverse('@@citations-json')
+        except AttributeError:
+            pass
+        return view
+
+    def testViewAvailableOnBibFolder(self):
+        ctxt = self.bib_folder
+        view = self.getView(ctxt)
+        self.assertTrue(view is not None)
+        self.assertTrue(isinstance(view, CitationJSONView))
+
+    def testViewOnBibFolderReturnsJSON(self):
+        ctxt = self.bib_folder
+        view = self.getView(ctxt)
+        json_val = view()
+        self.assertTrue(json_val)
+        try:
+            actual = json.loads(json_val)
+        except Exception:
+            self.fail('invalid json: %s' % json_val)
+        for obj in ctxt.objectValues():
+            expected = obj.UID()
+            self.assertTrue(expected in actual)
+
+    def testViewAvailableOnBibItems(self):
+        for ctxt in self.bib_folder.objectValues():
+            view = self.getView(ctxt)
+            self.assertTrue(view is not None)
+            self.assertTrue(isinstance(view, CitationJSONView))
+
+    def testViewOnBibItemsReturnsJSON(self):
+        for ctxt in self.bib_folder.objectValues():
+            view = self.getView(ctxt)
+            json_val = view()
+            self.assertTrue(json_val)
+            try:
+                actual = json.loads(json_val)
+            except Exception:
+                self.fail('invalid json: %s' % json_val)
+            self.assertTrue(ctxt.UID() in actual)
+
+    # XXX: This is needed, but we are skipping it for now because of the 
+    # old/new collections mess across Plone 4 versions
+    # 
+    # def testViewAvailableOnCollections(self):
+    #     ctxt = self._createCollection()
+    #     view = self.getView(ctxt)
+    #     self.assertTrue(view is not None)
+    #     self.assertTrue(isinstance(view, CitationJSONView))
+
+    
