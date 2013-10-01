22d1ffb8bc8221f0036124280f319c252e437d89
cewing <cris@crisewing.com>
2013-10-01 13:52:13 -0300

    remove breakpoint test for any bibliography type as context

--- src/collective/citationstyles/browser/viewlets.py
+++ src/collective/citationstyles/browser/viewlets.py
@@ -1,11 +1,16 @@
 from plone.app.layout.viewlets import common
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from Products.CMFBibliographyAT.interface import IBibliographicItem
+from Products.CMFBibliographyAT.interface import IBibliographyFolder
+from Products.CMFBibliographyAT.interface import ILargeBibliographyFolder
 
 
 class CiteprocSetupViewlet(common.ViewletBase):
     index = ViewPageTemplateFile('citeproc_setup.pt')
 
     def available(self):
-        import pdb; pdb.set_trace( )
-        return IBibliographicItem.providedBy(self.context)
+        if (IBibliographicItem.providedBy(self.context) or
+            IBibliographyFolder.providedBy(self.context) or
+            ILargeBibliographyFolder.providedBy(self.context)):
+            return True
+        return False
